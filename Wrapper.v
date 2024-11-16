a7cf6b013a778dc4e41ac54d320da1b8f98da79b
Joyce <jh829@duke.edu>
2024-11-16 16:33:34 -0500

    changed button for waiting period

--- Wrapper.v
+++ Wrapper.v
@@ -93,10 +93,12 @@ module Wrapper (input clk_100mhz, input red_button, input blue_button, input gre
 	light_up lights(.clock(clock), .flash_led(flash_led), .color(memDataIn[2:1]), .on_off(memDataIn[0]), .red_led(red_led), .blue_led(blue_led), .green_led(green_led), .yellow_led(yellow_led));
 
 	wire [31:0] button_out;
+	wire poll_button;
 	// if lw to address 7, calling check_buttons
+	assign poll_button = (memAddr[11:0] == 12'd7);
 	assign memDataOut = (memAddr[11:0] == 12'd7) ? button_out : memData_temp1;
 	// only assign memdataout to the actual color, and only if there was a button press?
-	button_press press(red_button, yellow_button, blue_button, green_button, clock, button_out);
+	button_press press(red_button, yellow_button, blue_button, green_button, clock, poll_button, button_out);
 
 	// TO DO: in mips, write thing that checks button presses, and turns on the led w button press
 	// in mips -> I will just shift the number myself to only get the two lsb (colors) or msb however i code this
--- io_devices/button_press.v
+++ io_devices/button_press.v
@@ -1,10 +1,16 @@
-module button_press(input red_button, input yellow_button, input blue_button, input green_button, input clock, output[31:0] button_out);
+module button_press(input red_button, input yellow_button, input blue_button, input green_button, input clock, input poll_button, output[31:0] button_out);
     // latch most recent button, until a new one is pressed
     // 00=red (0), 01=blue (1), 10=green (2), 11=yellow (3)
 
     // xx which button ... x was pressed, x was read 
     // 4 bit number.... xxxx
     reg [2:0] button_pressed; 
+    reg[31:0] counter;
+    wire [31:0] button_temp1;
+
+    wire first_cycle; 
+    assign first_cycle = button_pressed[0] & poll_button;
+
     always @(posedge clock) begin
         case({red_button, blue_button, green_button, yellow_button})
             4'b1000: button_pressed = 3'b001;
@@ -14,7 +20,16 @@ module button_press(input red_button, input yellow_button, input blue_button, in
             4'b0000: button_pressed = 3'b000;
         endcase
     end
-    assign button_out[31:3] = 29'd0; 
-    assign button_out[2:0] = button_pressed;
+    assign button_temp1[31:3] = 29'd0; 
+    assign button_temp1[2:0] = button_pressed;
+
+    assign button_out = (counter < 100000000) & (!first_cycle) ? 32'd0 : button_temp1;
+
+    always @(posedge clock) begin
+        if (poll_button == 1 && counter >= 100000000)
+            counter <= 1;
+        else if(counter > 0)
+            counter <= counter + 1;
+    end 
 
 endmodule
