a420e1c982a2e00f7e69f56c3e019dec8d8a9d1f
Joyce <jh829@duke.edu>
2024-11-16 22:58:04 -0500

    added delays

--- io_devices/button_press.v
+++ io_devices/button_press.v
@@ -2,8 +2,7 @@ module button_press(input red_button, input yellow_button, input blue_button, in
     // latch most recent button, until a new one is pressed
     // 00=red (0), 01=blue (1), 10=green (2), 11=yellow (3)
 
-    // xx which button ... x was pressed, x was read 
-    // 4 bit number.... xxxx
+    // last three bits of button out [2:1] = color, [0] = if presssed or not
     reg [2:0] button_pressed; 
     reg[31:0] counter;
     wire [31:0] button_temp1;
@@ -18,13 +17,15 @@ module button_press(input red_button, input yellow_button, input blue_button, in
             4'b0010: button_pressed = 3'b101;
             4'b0001: button_pressed = 3'b111;
             4'b0000: button_pressed = 3'b000;
+
         endcase
     end
     assign button_temp1[31:3] = 29'd0; 
     assign button_temp1[2:0] = button_pressed;
 
+    // putting a delay so the button presses don't get reread
+    // i also added one in the software so unsure if I need this
     assign button_out = (counter < 100000000) & (!first_cycle) ? 32'd0 : button_temp1;
-
     always @(posedge clock) begin
         if (poll_button == 1 && counter >= 100000000)
             counter <= 1;
