563c66c0ebe90bccd2db80adf1a7df6857aebbd0
Joyce Hu <jh829@duke.edu>
2024-12-02 21:36:02 -0500

    Minor servo edits

--- io_devices/ServoController.v
+++ io_devices/ServoController.v
@@ -1,25 +1,45 @@
 module ServoController(
     input        clk, 		     // System Clock Input 100 Mhz
-    input        useServo        // tells us that we want to use the servos
+    input        useServo,        // tells us that we want to use the servos
     input       [2:0] direction, // 000 goes nowhere, 001 forward, 010 backwards , 011 turn left, 100 turns right;
     output left_servo,
-    output right_servo );	
+    output right_servo);	
         
-    wire[9:0] duty_cycle_left, duty_cycle_right;
-    reg [2:0] direction_reg;
+    reg[9:0] duty_cycle_left, duty_cycle_right;
+    reg[2:0] direction_reg;
+    
     always @ (posedge clk) begin 
         if(useServo == 1'b1) begin
             direction_reg <= direction;
         end
     end
+    
     always @ (posedge clk) begin 
         case (direction_reg) 
-            3'b000: duty_cycle_left = 10'd28, duty_cycle_right = 10'd28;
-            3'b001: duty_cycle_left = 10'd50, duty_cycle_right = 10'd6; //forwards
-            3'b010: duty_cycle_left = 10'd6, duty_cycle_right = 10'd50; //backwards
-            3'b011: duty_cycle_left = 10'd6, duty_cycle_right = 10'd28; // turn left
-            3'b100: duty_cycle_left = 10'd50, duty_cycle_right = 10'd28; // turn right
-            default: duty_cycle_left = 10'd28, duty_cycle_right = 10'd28;
+            3'b000: begin
+                duty_cycle_left = 10'd28;
+                duty_cycle_right = 10'd28;
+            end
+            3'b001: begin
+                duty_cycle_left = 10'd50;  // forwards
+                duty_cycle_right = 10'd6;
+            end
+            3'b010: begin
+                duty_cycle_left = 10'd6;   // backwards
+                duty_cycle_right = 10'd50;
+            end
+            3'b011: begin
+                duty_cycle_left = 10'd6;   // turn left
+                duty_cycle_right = 10'd28;
+            end
+            3'b100: begin
+                duty_cycle_left = 10'd50;  // turn right
+                duty_cycle_right = 10'd28;
+            end
+            default: begin
+                duty_cycle_left = 10'd28;
+                duty_cycle_right = 10'd28;
+            end
         endcase
     end
 
