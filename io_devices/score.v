5ada0bd11f4030b1163381978377c9c306851285
Joyce Hu <jh829@duke.edu>
2024-12-04 02:02:35 -0500

    more display

--- io_devices/score.v
+++ io_devices/score.v
@@ -1,7 +1,7 @@
 module score(clock, change_score, number, cathode_1, cathode_2, cathode_3, top, top_right, bot_right, bot, bot_left, top_left, middle);
     input clock, change_score;
     input[9:0] number; // number to display on 3 digit 7seg
-    output cathode_1, cathode_2, cathode_3, top, top_right, bot_right, bot, bot_left, top_left, middle;
+    output reg cathode_1, cathode_2, cathode_3, top, top_right, bot_right, bot, bot_left, top_left, middle;
 
     reg [3:0] hundreds, tens, ones;  
     reg [1:0] current_digit;          
@@ -10,9 +10,9 @@ module score(clock, change_score, number, cathode_1, cathode_2, cathode_3, top, 
     // Splitting the number into each digit
     always @(posedge clock) begin
         if (change_score) begin
-            hundreds = number / 100;     
-            tens = (number % 100) / 10; 
-            units = number % 10;        
+            hundreds <= number / 100;     
+            tens <= (number % 100) / 10; 
+            ones <= number % 10;        
         end
     end
 
@@ -22,28 +22,28 @@ module score(clock, change_score, number, cathode_1, cathode_2, cathode_3, top, 
         current_digit <= current_digit + 1; 
         case (current_digit)
             2'b00: begin
-                cathode_1 = 1'b0;  
-                cathode_2 = 1'b1;
-                cathode_3 = 1'b1;
-                digit_value = hundreds;
+                cathode_1 <= 1'b0;  
+                cathode_2 <= 1'b1;
+                cathode_3 <= 1'b1;
+                digit_value <= hundreds;
             end
             2'b01: begin
-                cathode_1 = 1'b1;
-                cathode_2 = 1'b0; 
-                cathode_3 = 1'b1;
-                digit_value = tens;
+                cathode_1 <= 1'b1;
+                cathode_2 <= 1'b0; 
+                cathode_3 <= 1'b1;
+                digit_value <= tens;
             end
             2'b10: begin
-                cathode_1 = 1'b1;
-                cathode_2 = 1'b1;
-                cathode_3 = 1'b0;
-                digit_value = units;
+                cathode_1 <= 1'b1;
+                cathode_2 <= 1'b1;
+                cathode_3 <= 1'b0;
+                digit_value <= ones;
             end
         endcase
     end
 
     // 7 seg decoding
-    always @(*) begin
+    always @(posedge clock) begin
         case (digit_value)
             4'd0: {top, top_right, bot_right, bot, bot_left, top_left, middle} = 7'b1111110;
             4'd1: {top, top_right, bot_right, bot, bot_left, top_left, middle} = 7'b0110000;
